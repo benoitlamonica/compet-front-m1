fb891df41a062b2f0c144b79bac72d3f4f430a9e
Benoit Lamonica <ben33127@gmail.com>
2021-06-19 01:29:50 +0200

    Routes

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,6 +1,10 @@
 <template>
   <button class="button">
+<<<<<<< HEAD
     <a class="link" :href="link" :aria-label="ariaLabel">{{ text }}</a>
+=======
+    <router-link :to="link">{{ text }}</router-link>
+>>>>>>> 495d84a (Routes)
   </button>
 </template>
 
@@ -10,8 +14,12 @@ export default {
 
   props: {
     text: { type: String, required: true },
+<<<<<<< HEAD
     link: { type: URL, required: true },
     ariaLabel: { type: String, required: true },
+=======
+    link: { type: String, required: false },
+>>>>>>> 495d84a (Routes)
   },
 };
 </script>
---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import App from '@/App.vue'
+import Game from '@/pages/Game.vue'
+
+export const routes = [
+    {
+        path: '/',
+        component: App
+    },
+    {
+        path: '/game',
+        component: Game
+    }
+]
