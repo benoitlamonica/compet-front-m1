ae7640467482ccaa9903825232f03eb8c76401b6
Benoit Lamonica <ben33127@gmail.com>
2021-06-19 19:56:04 +0200

    Router + Api

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
-    <p>coucou</p>
+    <div v-if="allMainIng != null" class="ing-list">
+      <div v-for="ing in allMainIng" :key="ing.id" class="ing-single">
+        <p>{{ ing.name }}</p>
+      </div>
+    </div>
     <router-link to="/">Retour</router-link>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Game",
+  data() {
+    return {
+      allMainIng: null,
+    };
+  },
+  mounted() {
+    axios.get("http://127.0.0.1:8000/ingredient/main").then((rep) => {
+      this.allMainIng = rep.data;
+    });
+  },
 };
 </script>
 
